34c3c2a51c7dae4953d89ddc13fe42c5c200b6ce
Fezz <jonnicwolf@gmail.com>
2024-02-17 07:20:45 -0500

    moves board obj to its own file

--- src/pieceMovements/bishop.js
+++ src/pieceMovements/bishop.js
@@ -1,127 +1,3 @@
-const pieces = {
-  black: {
-    bishop: 'src/assets/pieces/black/bishop.png',
-    king: 'src/assets/pieces/black/king.png',
-    knight: 'src/assets/pieces/black/knight.png',
-    queen: 'src/assets/pieces/black/queen.png',
-    pawn: 'src/assets/pieces/black/pawn.png',
-    rook: 'src/assets/pieces/black/rook.png',
-  },
-  white: {
-    bishop: 'src/assets/pieces/white/bishop.png',
-    king:   'src/assets/pieces/white/king.png',
-    knight: 'src/assets/pieces/white/knight.png',
-    queen:  'src/assets/pieces/white/queen.png',
-    pawn:   'src/assets/pieces/white/pawn.png',
-    rook:   'src/assets/pieces/white/rook.png',
-  },
-};
-const {black, white} = pieces;
-const starting_position = {
-    1: [
-      {position: [1,1], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [1,2], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [1,3], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [1,3], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [1,4], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [1,5], piece: {pieceType: null, pieceImgPath: null}} ],
-    2: [
-      {position: [2,1], piece: {pieceType: 'pawn', pieceImgPath: black.pawn}},
-      {position: [2,2], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [2,3], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [2,4], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [2,5], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [2,6], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [2,7], piece: {pieceType: 'pawn', pieceImgPath: white.pawn}} ],
-    3: [
-      {position: [3,1], piece: {pieceType: 'rook', pieceImgPath: black.rook}},
-      {position: [3,2], piece: {pieceType: 'pawn', pieceImgPath: black.pawn}},
-      {position: [3,3], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [3,4], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [3,5], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [3,6], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [3,7], piece: {pieceType: 'pawn', pieceImgPath: white.pawn}},
-      {position: [3,8], piece: {pieceType: 'rook', pieceImgPath: white.rook}} ],
-    4: [
-      {position: [4,1], piece: {pieceType: 'b_knight', pieceImgPath: black.knight}},
-      {position: [4,2], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [4,3], piece: {pieceType: 'pawn', pieceImgPath: black.pawn}},
-      {position: [4,4], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [4,5], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [4,6], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [4,7], piece: {pieceType: 'pawn', pieceImgPath: white.pawn}},
-      {position: [4,8], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [4,9], piece: {pieceType: 'knight', pieceImgPath: white.pawn}} ],
-    5: [
-      {position: [5,1], piece: {pieceType: 'queen', pieceImgPath: black.queen}},
-      {position: [5,2], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [5,3], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [5,4], piece: {pieceType: 'pawn', pieceImgPath: black.pawn}},
-      {position: [5,5], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [5,6], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [5,7], piece: {pieceType: 'pawn', pieceImgPath: white.pawn}},
-      {position: [5,8], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [5,9], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [5,10], piece: {pieceType: 'queen', pieceImgPath: white.queen}} ],
-    6: [
-      {position: [6,1], piece: {pieceType: 'bishop', pieceImgPath: black.bishop}},
-      {position: [6,2], piece: {pieceType: 'bishop', pieceImgPath: black.bishop}},
-      {position: [6,3], piece: {pieceType: 'bishop', pieceImgPath: black.bishop}},
-      {position: [6,4], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [6,5], piece: {pieceType: 'pawn', pieceImgPath: black.pawn}},
-      {position: [6,6], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [6,7], piece: {pieceType: 'pawn', pieceImgPath: white.pawn}},
-      {position: [6,8], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [6,9], piece: {pieceType: 'bishop', pieceImgPath: white.bishop}},
-      {position: [6,10], piece: {pieceType: 'bishop', pieceImgPath: white.bishop}},
-      {position: [6,11], piece: {pieceType: 'bishop', pieceImgPath: white.bishop}} ],
-    7: [
-      {position: [7,1], piece: {pieceType: 'king', pieceImgPath: black.king}},
-      {position: [7,2], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [7,3], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [7,4], piece: {pieceType: 'pawn', pieceImgPath: black.pawn}},
-      {position: [7,5], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [7,6], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [7,7], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [7,8], piece: {pieceType: 'pawn', pieceImgPath: white.pawn}},
-      {position: [7,9], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [7,10], piece: {pieceType: 'king', pieceImgPath: white.king}} ],
-    8: [
-      {position: [8,1], piece: {pieceType: 'b_knight', pieceImgPath: black.knight}},
-      {position: [8,2], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [8,3], piece: {pieceType: 'pawn', pieceImgPath: black.pawn}},
-      {position: [8,4], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [8,5], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [8,6], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [8,7], piece: {pieceType: 'pawn', pieceImgPath: white.pawn}},
-      {position: [8,8], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [8,9], piece: {pieceType: 'knight', pieceImgPath: white.knight}} ],
-    9: [
-      {position: [9,1], piece: {pieceType: 'rook', pieceImgPath: black.rook}},
-      {position: [9,2], piece: {pieceType: 'pawn', pieceImgPath: black.pawn}},
-      {position: [9,3], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [9,4], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [9,5], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [9,6], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [9,7], piece: {pieceType: 'pawn', pieceImgPath: white.pawn}},
-      {position: [9,8], piece: {pieceType: 'rook', pieceImgPath: white.rook}} ],
-    10: [
-      {position: [10,1], piece: {pieceType: 'pawn', pieceImgPath: black.pawn}},
-      {position: [10,2], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [10,3], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [10,4], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [10,5], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [10,6], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [10,7], piece: {pieceType: 'pawn', pieceImgPath: white.pawn}} ],
-    11: [
-      {position: [11,1], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [11,2], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [11,3], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [11,4], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [11,5], piece: {pieceType: null, pieceImgPath: null}},
-      {position: [11,6], piece: {pieceType: null, pieceImgPath: null}} ],
-};
-
 export const bishop_move = (activeCells, board) => {
   const a_coordinates = activeCells[0];
   const a_cell        = a_coordinates[1];
@@ -167,7 +43,7 @@ export const bishop_move = (activeCells, board) => {
 
   let rightmost_cell_2 = [];
   let leftmost_cell_2_clone = [...leftmost_cell_2];
-  
+
   do {
     if ( board[leftmost_cell_2_clone[0]][leftmost_cell_2_clone[1]] ) {
       if (leftmost_cell_2_clone[0] < 6) {
@@ -218,5 +94,3 @@ export const bishop_move = (activeCells, board) => {
 
   return [...f_line_1, ...f_line_2, ...f_line_3];
 };
-
-console.log(bishop_move( [[7,6],[5,2]], starting_position ));
